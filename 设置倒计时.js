b1ab3d4e2e862cc4b734d0eff35174cab47a6de0
LinBoyan <69086800+LinBoyan@users.noreply.github.com>
2023-09-04 16:20:00 +0800

    Add files via upload

---
+++ 设置倒计时.js
@@ -0,0 +1,35 @@
+import plugin from "../../lib/plugins/plugin.js";
+import common from "../../lib/common/common.js";
+export class countdown extends plugin {
+    constructor() {
+        super({
+            name: "countdown",
+            dsc: "设置倒计时",
+            event: "message",
+            priority: 4096,
+            rule: [
+                {
+                    reg: "^#?(.*)?(小时)?(.*)?(分钟)后叫我$",
+                    fnc: "countdown"
+                }
+            ]
+        })
+    }
+    async call(e){
+    
+        await e.reply(`时间到啦！`, true, { at: true })
+    }
+    async countdown(e){
+    
+        let minute = e.msg.match(/\d+分钟/) || 0
+        minute = parseInt(minute)
+
+        let hour = e.msg.match(/\d+小时/) || 0
+        hour = parseInt(hour)
+
+        let delay = (hour*60 + minute)*60*1000
+
+        await this.e.reply(`好的，${hour}小时${minute}分钟后叫你`, true, { at: true })
+        setTimeout(this.call,delay,e)
+    }
+}
