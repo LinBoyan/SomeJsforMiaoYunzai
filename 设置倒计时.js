652217f0656ea80ac4993dc70a7937c499fc85ff
LinBoyan <69086800+LinBoyan@users.noreply.github.com>
2023-09-08 01:38:55 +0800

    Update via upload
    加入@别人

--- 设置倒计时.js
+++ 设置倒计时.js
@@ -9,18 +9,19 @@ export class countdown extends plugin {
             priority: 4096,
             rule: [
                 {
-                    reg: "^#(.*)(小时)?(.*)(分钟)?后叫我(.*)",
+                    reg: "^#(.*)(小时)?(.*)(分钟)?后叫(我)?(.*)",
                     fnc: "countdown"
                 }
             ]
         })
     }
-    async call(e,action){
-    
-        await e.reply(`时间到啦！${action}!`, true, { at: true })
+    async call(e,action,signal,qq){
+        if(signal)
+            await e.reply(`时间到啦！${action}!`, true, { at: true })
+        else
+            await e.reply([segment.at(+qq),`时间到啦！${action}!`], true)
     }
     async countdown(e){
-    
         let minute = e.msg.match(/\d+分钟/) || 0
         minute = parseInt(minute)
 
@@ -29,9 +30,23 @@ export class countdown extends plugin {
 
         let delay = (hour*60 + minute)*60*1000
 
-        let action = e.msg.replace(/#(.*)(小时)?(.*)(分钟)?后叫我/,"")
+        let signal = 0
+        let action
+        if(e.msg.match(/#(.*)(小时)?(.*)(分钟)?后叫我/)){
+            signal = 1
+            action = e.msg.replace(/#(.*)(小时)?(.*)(分钟)?后叫我/,"")
+        }
+        else
+            action = e.msg.replace(/#(.*)(小时)?(.*)(分钟)?后叫/,"")
+        
+        let at = e.message.filter(item => item.type == 'at')?.map(item => item?.qq);
+        let qq = at[0];
 
-        await this.e.reply(`好的，${hour}小时${minute}分钟后叫你${action}`, true, { at: true })
-        setTimeout(this.call,delay,e,action)
+        if(signal)
+            await this.e.reply(`好的，${hour}小时${minute}分钟后叫你${action}`, true)
+        else
+            await this.e.reply([`好的，${hour}小时${minute}分钟后叫`,segment.at(+qq),action], true)
+        
+        setTimeout(this.call,delay,e,action,signal,qq)
     }
 }
