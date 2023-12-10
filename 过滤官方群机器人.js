2f01417e8a678393a43ae3f8fa9a06be84c8fb6e
岩浆 <linboyan5438081@163.com>
2023-12-10 23:19:13 +0800

    让野生机器人屏蔽@官方机器人的消息
    因为官方群机器人只能@触发，故屏蔽@2854和3889号段的消息，净化群内环境

---
+++ 过滤官方群机器人.js
@@ -0,0 +1,38 @@
+let ignone_qq = [2337095165,2854216359,2854198952]//手工过滤的机器人
+export class ignore_bot extends plugin {
+    constructor() {
+        super({
+            name: "ignore_bot",
+            dsc: "手工过滤机器人",
+            event: "message",
+            priority: -999999,
+            rule: [
+                {
+                    reg: "",
+                    fnc: "ignore_bot",
+                    log: false
+                },
+                {
+                    reg: "^#?看QQ号",
+                    fnc: "check_id",
+                    log: false
+                },
+            ]
+        })
+    }
+    async ignore_bot(e){
+        if(ignone_qq.includes(e.at) || ignone_qq.includes(e.user_id))
+            return;
+
+        //按号码段过滤群机器人
+        else if((e.at>2854000000 && e.at<2855000000) || (e.user_id>2854000000 && e.user_id<2855000000))
+            return;
+        else if((e.at>3889000000 && e.at<3890000000) || (e.user_id>3889000000 && e.user_id<3890000000))
+            return;
+        else 
+            return false;
+    }
+    async check_id(e){
+        this.reply(e.at)
+    }
+}
