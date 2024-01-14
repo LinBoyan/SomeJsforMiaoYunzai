811014be3a378cb7b754e4a4288ade23f8304240
岩浆 <linboyan5438081@163.com>
2024-01-14 21:40:44 +0000

    需要Markdown模板和野生icqq

---
+++ openid取qq.js
@@ -0,0 +1,131 @@
+let form = {}
+import fs from 'fs'
+import yaml from 'yaml'
+const authors = ['岩浆']
+const folderPath = './plugins/example/' //数据存放路径
+const prefix = '' //野生机器人前缀
+const Delay_ms = 2000 //指令延时
+const group = 514170328
+const QQBot_qq = 2854216359
+
+export class OpenIdtoId extends plugin {
+  constructor () {
+    super({
+      name: '取qq号',
+      dsc: '复读用户发送的内容，然后撤回',
+      event: 'message',
+      priority: -50001,
+      rule: [
+        {
+          reg: '^#?开始转换(qq|QQ)号$',
+          fnc: 'sendOpenid',
+          permission: "master"
+        },
+        {
+          reg: '^#?对应关系',
+          fnc: 'writeOpenid'
+        },
+        {
+          reg: '^#?启动对应转换',
+          fnc: 'startOpenid'
+        },
+        {
+          reg: '^#?身份查询',
+          fnc: 'transformer'
+        },
+      ]
+    })
+    this.task = {
+      name: '控制icqq发起转换',
+      fnc: () => this.startOpenid(),
+      cron: `0 2 * * *`
+    }
+  }
+
+  async startOpenid () {
+    Bot.pickGroup(group).sendMsg([segment.at(QQBot_qq),'开始转换qq号'])
+  }
+
+  async transformer (e) {
+    let search_id  = e.msg.replace(/^#?身份查询/,'').replace(/ /g, '')
+    let openid
+    if (fs.existsSync(`${folderPath}QQBotRelation/${e.self_id}.yaml`))
+      form = yaml.parse(fs.readFileSync(`${folderPath}QQBotRelation/${e.self_id}.yaml`, 'utf8'))
+    if (search_id.match(`${e.self_id}-`)) {
+      if(form[search_id]?.qq)
+        openid = search_id
+    }
+    else {
+      for (let User in form)
+        if (form[User].qq == (+search_id))
+          openid = User
+    }
+    if (openid == undefined) 
+      this.reply(`暂未收录`)
+    else 
+      this.reply(`\r#身份查询 结果\r\rUserID: ${openid}\r\r>QQ: ${form[openid].qq}\r\r>昵称: ${form[openid].nickname}`)
+    return
+  }
+
+  async sendOpenid (e){
+    await this.reply(`${prefix}对应关系发送开始${e.self_id}`)
+    await sleep(Delay_ms)
+    let today = new Date().getTime()
+    today = new Date(today - 1 * 24 * 60 * 60 * 1000).toLocaleDateString()
+    const data = fs.readFileSync(`${folderPath}user_id.yaml`, 'utf8')
+    let user_list = yaml.parse(data)
+    user_list = user_list[today]
+    let msg = `${prefix}对应关系`
+    let i = 0
+    for (const openid of user_list){
+      if(openid.match(`${e.self_id}-`)){
+        msg = msg + `\r${openid}<@${openid.replace(`${e.self_id}-`,'')}>`
+        i++
+        if(i == 50){
+          this.reply(msg)
+          msg = `${prefix}对应关系`
+          i = 0
+          await sleep(Delay_ms)
+        }
+      }
+    }
+    await this.reply(msg)
+    await sleep(Delay_ms)
+    await this.reply(`${prefix}对应关系发送完成${e.self_id}`)
+  }
+
+  async writeOpenid (e) {
+    if (e.msg.match(/对应关系发送开始/)){
+      const Botid = e.msg.replace(/对应关系发送开始/,'')
+      const filePath = `${folderPath}QQBotRelation`
+      if (fs.existsSync(`${filePath}/${Botid}.yaml`))
+        form = yaml.parse(fs.readFileSync(`${filePath}/${Botid}.yaml`, 'utf8'))
+      else 
+        fs.mkdirSync(filePath, { recursive: true })
+      return
+    }
+    if (e.msg.match(/对应关系发送完成/)){
+      logger.mark(form)
+      const Botid = e.msg.replace(/对应关系发送完成/,'')
+      const filePath = `${folderPath}QQBotRelation/${Botid}.yaml`
+      fs.writeFileSync(filePath, yaml.stringify(form), 'utf8')
+      this.reply(`写入对应关系${filePath}`)
+      form = {}
+      return
+    }
+    for (let openid = 1; openid < e.message.length; openid+=2) {
+      let id = e.message[openid].text
+      .replace(`对应关系`, '')
+      .replace(`\r`, '')
+      .replace(`\n`, '')
+      .replace(` `, '')
+      form[id] = {}
+      form[id].qq = e.message[openid + 1].qq
+      form[id].nickname = e.message[openid + 1].text.replace(/\@/, '')
+    }
+  }
+}
+
+function sleep (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
